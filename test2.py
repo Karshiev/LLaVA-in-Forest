50c1947bc5ed54ced71fc5343c1bc9505d652d30
Karshiev <karshiev.uzb@gmail.com>
2024-08-14 10:19:38 +0900

    Data preparation

--- test2.py
+++ test2.py
@@ -2,19 +2,20 @@ from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path
 from llava.eval.run_llava import eval_model
 
+import torch
+
 import warnings
 warnings.filterwarnings('ignore')
 
-
+import PIL.Image as Image
 model_path = "liuhaotian/llava-v1.5-7b"
-prompt = "What are the things I should be cautious about when I visit here?"
-image_file = "./datasets/amazon/train/train-jpg/train_0.jpg"
+prompt = "Give me a description of this image. Describe the types of land use found at the center-bottom of the image."
+image_file = "/ssd_1/sanjar/rsvlm/datasets/amazon/train/train-jpg/train_1.jpg"
 
-print(get_model_name_from_path(model_path))
-
+# image_file = "https://llava-vl.github.io/static/images/view.jpg"
 
 args = type('Args', (), {
-    "model_path": 'checkpoints/original',
+    "model_path": model_path,
     "model_base": None,
     "model_name": get_model_name_from_path(model_path),
     "query": prompt,
