4c06ea4b606b257c1295924c4044b2cbd2af1d63
Karshiev <karshiev.uzb@gmail.com>
2024-08-24 10:38:15 +0900

    Add few-shot-all.json

--- scripts/v1_5/finetune_task_lora.sh
+++ scripts/v1_5/finetune_task_lora.sh
@@ -3,10 +3,10 @@
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path liuhaotian/llava-v1.5-13b \
+    --model_name_or_path MBZUAI/geochat-7B \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path /ssd_1/sanjar/rsvlm/datasets/region_grounded_mix.json\
+    --image_folder /ssd_1/sanjar/rsvlm/datasets\
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -14,22 +14,22 @@ deepspeed llava/train/train_mem.py \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --bf16 False \
+    --output_dir ./checkpoints/Reg-Gr/test_just\
+    --num_train_epochs 10 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 2e-4 \
+    --learning_rate 1e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
